2cc38d1fa47ec5af2fbf40edf24409d370c8f706
jeongsoolee09 <volcano0909@gmail.com>
2021-11-14 15:02:51 +0900

    add smart_pairup_vertices

--- lib/SimilarityHandler.ml
+++ lib/SimilarityHandler.ml
@@ -141,36 +141,62 @@ module SimilarVertexPairExtractor = struct
           @ bidirectional_carpro @ redefines_carpro
 
 
+    let with_list_index (lst : 'a list) : (int * 'a) list =
+      List.rev @@ List.foldi ~f:(fun index acc elem -> (index, elem) :: acc) ~init:[] lst
+
+
+    (** pairup similar vertices, also putting list indices into consideration. e.g. if a and b are
+        similar, and if list_a = [a; c; d; a; a; a;] and list_b = [f; g; h; b; b; i; j], then pair
+        up second and third occurrences of a's with first and second occurrences of b's. *)
+    let smart_pairup_vertices (trunk_a : trunk) (trunk_b : trunk)
+        (similar_methods : (string * string) list) =
+      let trunk_a_processed = with_list_index (List.stable_dedup trunk_a)
+      and trunk_b_processed = with_list_index (List.stable_dedup trunk_b) in
+      if Int.( >= ) (List.length trunk_a) (List.length trunk_b) then
+        (* loop on trunk_b *)
+        List.fold
+          ~f:(fun acc ((b_index, b_elem) as b_tuple) ->
+            (* find the a_tuple in trunk_a with the minimum difference in terms of index *)
+            let a_elem_with_smallest_diff =
+              List.fold
+                ~f:(fun ((current_min, _) as current_min_tuple) ((a_index, a_elem) as a_tuple) ->
+                  let diff = Int.abs (Int.( - ) a_index b_index) in
+                  if diff <= current_min then a_tuple else current_min_tuple )
+                ~init:(Int.max_value, ("", ""))
+                trunk_a_processed
+            in
+            (b_elem, snd a_elem_with_smallest_diff) :: acc )
+          ~init:[] trunk_b_processed
+      else
+        (* loop on trunk_a *)
+        List.fold
+          ~f:(fun acc ((a_index, a_elem) as a_tuple) ->
+            (* find the b_tuple in trunk_b with the minimum difference in terms of index *)
+            let b_elem_with_smallest_diff =
+              List.fold
+                ~f:(fun ((current_min, _) as current_min_tuple) ((b_index, b_elem) as b_tuple) ->
+                  let diff = Int.abs (Int.( - ) b_index a_index) in
+                  if diff <= current_min then b_tuple else current_min_tuple )
+                ~init:(Int.max_value, ("", ""))
+                trunk_b_processed
+            in
+            (a_elem, snd b_elem_with_smallest_diff) :: acc )
+          ~init:[] trunk_a_processed
+
+
     let init_contextual_score_map (all_trunks : trunk list) : NodeWiseSimilarityScoreMap.t =
       let carpro =
         let* trunk1 = all_trunks in
         let* trunk2 = all_trunks in
         return (trunk1, trunk2)
       in
-      (* now, we need to translate the trunk similarity into node similarity. *)
+      (* now, we need to translate the trunk similarity into method similarity. *)
       raise TODO
   end
 end
 
 module EstablishSimEdges = struct
   (* since SimlarVertexPairExtractor extracts only pairs of similar methods, we need to find vertices containing those similar methods. *)
-
-  let with_list_index (lst : 'a list) : ('a * int) list =
-    List.rev @@ List.foldi ~f:(fun index acc elem -> (index, elem) :: acc) ~init:[] lst
-
-
-  (** pairup similar vertices, also putting list indices into consideration. e.g. if a and b are
-      similar, and if list_a = [a; c; d; a; a; a;] and list_b = [f; g; h; b; b; i; j], then pair up
-      second and third occurrences of a's with first and second occurrences of b's. *)
-  let smart_pairup_vertices (trunk_a : trunk) (trunk_b : trunk)
-      (similar_methods : (string * string) list) =
-    let trunk_a_processed = trunk_a |> List.stable_dedup >>| with_list_index
-    and trunk_b_processed = trunk_b |> List.stable_dedup >>| with_list_index in
-    if Int.( >= ) (List.length trunk_a) (List.length trunk_b) then (* loop on trunk_b *)
-      List.fold ~f:(fun acc b_elem -> ) ~init:[] trunk_b
-    else (* loop on trunk_a *)
-      List.fold ~f:(fun acc a_elem -> ) ~init:[] trunk_a
-
 
   let make_nodewise_sim_edge (graph : G.t) : G.t =
     let open SimilarVertexPairExtractor in
