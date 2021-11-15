3918572c2f037d116e31ae31579bc0533d6e7e52
jeongsoolee09 <volcano0909@gmail.com>
2021-11-15 16:46:19 +0900

    Test code
    ...which is useless on its own and should be used wiht the REPL

--- test/InferenceEngineTests.ml
+++ test/InferenceEngineTests.ml
@@ -1,1 +1,28 @@
-let () = print_endline "hihi"
+open GraphRepr
+open ContextualFeatures
+
+let ( >>| ) = List.( >>| )
+
+let ( >>= ) = List.( >>= )
+
+let return = List.return
+
+let sample_graph =
+  G.empty
+  |> (fun graph -> G.add_edge graph ("nextLine", "26") ("create", "26"))
+  |> (fun graph -> G.add_edge graph ("create", "26") ("create", "31"))
+  |> (fun graph -> G.add_edge graph ("create", "31") ("run", "31"))
+  |> (fun graph -> G.add_edge graph ("run", "31") ("append", "32"))
+  |> (fun graph -> G.add_edge graph ("append", "32") ("run", "32"))
+  |> (fun graph -> G.add_edge graph ("run", "32") ("append", "32"))
+  |> (fun graph -> G.add_edge graph ("run", "32") ("toString", "32"))
+  |> (fun graph -> G.add_edge graph ("toString", "32") ("run", "32"))
+  |> (fun graph -> G.add_edge graph ("run", "32") ("batchUpdate", "34"))
+  |> fun graph -> G.add_edge graph ("run", "33") ("batchUpdate", "34")
+
+
+(** How will identify_trunk work on sample_graph? *)
+
+let _ =
+  let trunks = identify_trunks sample_graph in
+  List.iter ~f:(fun trunk -> Trunk.pp trunk) trunks
