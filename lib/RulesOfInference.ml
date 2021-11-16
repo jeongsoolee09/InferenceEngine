c3c19bc074b1de15401c030571c08d8635638aad
jeongsoolee09 <volcano0909@gmail.com>
2021-11-16 18:57:43 +0900

    update RulesOfInference

--- lib/RulesOfInference.ml
+++ lib/RulesOfInference.ml
@@ -4,6 +4,8 @@ open Probability
 
 exception TODO
 
+exception NotImplemented
+
 module Response = struct
   type t = string * TaintLabel.t
 
@@ -19,7 +21,40 @@ module PropagationRules = struct
       (prev_facts : Response.t list) (graph : G.t) : ProbMap.t =
     let new_fact_method = Response.get_method new_fact
     and new_fact_label = Response.get_label new_fact in
-    raise TODO
+    let new_fact_method_vertices =
+      G.all_vertices_of_graph graph
+      |> List.filter ~f:(fun (meth, _) -> String.equal meth new_fact_method)
+    in
+    let contextual_succs = new_fact_method_vertices >>= fun vertex -> Utils.cs_succs vertex graph in
+    let contextual_succ_dist = contextual_succs >>| fun succ -> ProbMap.find succ distmap in
+    List.fold
+      ~f:(fun acc succ ->
+        let succ_dist = ProbMap.find succ distmap in
+        let new_dist =
+          match new_fact_label with
+          | Source | Sink | Sanitizer ->
+              if G.is_root succ graph then
+                { ProbQuadruple.src= succ_dist.src +. 0.3
+                ; sin= succ_dist.sin -. 0.1
+                ; san= succ_dist.san -. 0.1
+                ; non= succ_dist.non -. 0.1 }
+              else if G.is_leaf succ graph then
+                { ProbQuadruple.src= succ_dist.src -. 0.1
+                ; sin= succ_dist.sin -. 0.1
+                ; san= succ_dist.san -. 0.1
+                ; non= succ_dist.non +. 0.3 }
+              else
+                { ProbQuadruple.src= succ_dist.src -. 0.1
+                ; sin= succ_dist.sin -. 0.1
+                ; san= succ_dist.san +. 0.3
+                ; non= succ_dist.non -. 0.1 }
+          | None ->
+              raise NotImplemented
+          | Indeterminate ->
+              failwith "Impossible"
+        in
+        ProbMap.strong_update succ succ_dist acc )
+      contextual_succs ~init:distmap
 
 
   (** Propagate the same info to nodes that are similar nodewise. *)
@@ -33,17 +68,40 @@ module PropagationRules = struct
     in
     let similarity_succs = new_fact_method_vertices >>= fun vertex -> Utils.ns_succs vertex graph in
     let similarity_succ_dist = similarity_succs >>| fun succ -> ProbMap.find succ distmap in
-    match new_fact_label with
-    | Source ->
-        raise TODO
-    | Sink ->
-        raise TODO
-    | Sanitizer ->
-        raise TODO
-    | None ->
-        raise TODO
-    | Indeterminate ->
-        failwith "Impossible"
+    List.fold
+      ~f:(fun acc succ ->
+        let succ_dist = ProbMap.find succ distmap in
+        let new_dist =
+          match new_fact_label with
+          | Source ->
+              (* bump the likelihood of the successor being a source *)
+              { ProbQuadruple.src= succ_dist.src +. 0.3
+              ; sin= succ_dist.sin -. 0.1
+              ; san= succ_dist.san -. 0.1
+              ; non= succ_dist.non -. 0.1 }
+          | Sink ->
+              (* bump the likelihood of the successor being a source *)
+              { ProbQuadruple.src= succ_dist.src -. 0.1
+              ; sin= succ_dist.sin +. 0.3
+              ; san= succ_dist.san -. 0.1
+              ; non= succ_dist.non -. 0.1 }
+          | Sanitizer ->
+              (* bump the likelihood of the successor being a source *)
+              { ProbQuadruple.src= succ_dist.src -. 0.1
+              ; sin= succ_dist.sin -. 0.1
+              ; san= succ_dist.san +. 0.3
+              ; non= succ_dist.non -. 0.1 }
+          | None ->
+              (* bump the likelihood of the successor being a source *)
+              { ProbQuadruple.src= succ_dist.src -. 0.1
+              ; sin= succ_dist.sin -. 0.1
+              ; san= succ_dist.san -. 0.1
+              ; non= succ_dist.non +. 0.3 }
+          | Indeterminate ->
+              failwith "Impossible"
+        in
+        ProbMap.strong_update succ succ_dist acc )
+      similarity_succs ~init:distmap
 
 
   let annotation_rule (distmap : ProbMap.t) (new_fact : Response.t) (prev_facts : Response.t list)
