b5c3ab9bd51c15923ff0f4bb84dbe8b0265a8233
이정수 <jslee@ijeongsus-MacBook-Air.local>
2021-11-21 18:33:35 +0900

    dont' call main, since we'll use utop

--- bin/InferenceEngineApplication.ml
+++ bin/InferenceEngineApplication.ml
@@ -7,7 +7,7 @@ let main () =
   let initial_distmap = Probability.make_map_for_graph graph
   and nodewise_featuremap = NodeWiseFeatures.init_feature_map graph in
   let interaction_completed = Loop.loop initial_distmap [] graph nodewise_featuremap in
-  raise TODO
+  print_endline "hihi"
 
 
-let _ = main ()
+(* let _ = main () *)
