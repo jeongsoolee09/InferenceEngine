fadb5d1c646a459fbd9e30d715c80d2d86eb08c3
jeongsoolee09 <volcano0909@gmail.com>
2021-11-22 12:49:13 +0900

    add a missing argument

--- bin/InferenceEngineApplication.ml
+++ bin/InferenceEngineApplication.ml
@@ -6,7 +6,7 @@ let main () =
   let graph = Deserializer.deserialize_json () |> GraphMaker.init_graph in
   let initial_distmap = Probability.make_map_for_graph graph
   and nodewise_featuremap = NodeWiseFeatures.init_feature_map graph in
-  let interaction_completed = Loop.loop initial_distmap [] graph nodewise_featuremap in
+  let interaction_completed = Loop.loop initial_distmap [] graph nodewise_featuremap 1 in
   print_endline "hihi"
 
 
