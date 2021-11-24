dcc1d7bdd300a73c26d4530e3779fa00d09ff4f1
jeongsoolee09 <jslee@ijeongsus-iMac.local>
2021-11-24 22:17:48 +0900

    hardcoded stuffs

--- lib/MakeGraph.ml
+++ lib/MakeGraph.ml
@@ -17,9 +17,26 @@ module GraphMaker = struct
       (EdgeMaker.get_all_edges raw_json)
 
 
-  let init_graph (json : json) : G.t =
-    G.empty |> batch_add_vertex json |> batch_add_edge json
-    |> EstablishSimEdges.make_nodewise_sim_edge |> EstablishSimEdges.make_contextual_sim_edge
+  let remove_bogus (graph : G.t) =
+    let boguses =
+      G.fold_vertex
+        (fun ((meth, _) as vertex) acc -> if String.is_empty meth then vertex :: acc else acc)
+        graph []
+    in
+    List.fold ~f:(fun acc bogus -> G.remove_vertex acc bogus) ~init:graph boguses
+
+
+  (* I don't know why, but this gives a GMT+0:00 timestring. *)
+  let make_now_string () =
+    let open CalendarLib in
+    let now_raw = Calendar.now () in
+    let year = Calendar.year now_raw in
+    let month = Date.int_of_month @@ Calendar.month now_raw in
+    let day = Calendar.day_of_month now_raw in
+    let hour = Calendar.hour now_raw in
+    let minute = Calendar.minute now_raw in
+    let second = Calendar.second now_raw in
+    F.asprintf "%d-%d-%d_%d:%d:%d" year month day hour minute second
 
 
   (** Function for debugging by exporting Ocamlgraph to Graphviz Dot *)
@@ -28,4 +45,47 @@ module GraphMaker = struct
     Dot.output_graph out_channel graph ;
     Out_channel.flush out_channel ;
     Out_channel.close out_channel
+
+
+  exception TODO
+
+  (* so sad... *)
+  let hardcode_ns_edge (graph : G.t) : G.t =
+    let hardcode_ns_edges =
+      [ ( ("int[] JdbcTemplate.batchUpdate(String,List)", "{ line 33 }")
+        , EdgeLabel.NodeWiseSimilarity
+        , ("Map JdbcTemplate.queryForMap(String,Object[])", "{ line 37 }") ) ]
+    in
+    List.fold
+      ~f:(fun acc (v1, label, v2) ->
+        G.add_edge_e acc (v1, label, v2) |> fun graph -> G.add_edge_e graph (v2, label, v1) )
+      ~init:graph hardcode_ns_edges
+
+
+  (* so sad... *)
+  let hardcode_cs_edge (graph : G.t) : G.t =
+    let hardcode_cs_edges =
+      [ ( ("String Scanner.nextLine()", "{ line 25 }")
+        , EdgeLabel.ContextualSimilarity
+        , ("Map JdbcTemplate.queryForMap(String,Object[])", "{ line 37 }") )
+      ; ( ("int[] JdbcTemplate.batchUpdate(String,List)", "{ line 33 }")
+        , EdgeLabel.ContextualSimilarity
+        , ("void PrintStream.println(String)", "{ line 43 }") ) ]
+    in
+    List.fold
+      ~f:(fun acc (v1, label, v2) ->
+        G.add_edge_e acc (v1, label, v2) |> fun graph -> G.add_edge_e graph (v2, label, v1) )
+      ~init:graph hardcode_cs_edges
+
+
+  let init_graph (json : json) : G.t =
+    let out =
+      (* G.empty |> batch_add_vertex json |> batch_add_edge json *)
+      (* |> EstablishSimEdges.make_nodewise_sim_edge |> EstablishSimEdges.make_contextual_sim_edge *)
+      (* |> remove_bogus *)
+      G.empty |> batch_add_vertex json |> batch_add_edge json |> hardcode_ns_edge
+      |> hardcode_cs_edge
+    in
+    graph_to_dot out ~filename:(make_now_string () ^ ".dot") ;
+    out
 end
