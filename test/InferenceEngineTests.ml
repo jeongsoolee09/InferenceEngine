85fef9f0811461d625c3a46d9f09e37502a36097
jeongsoolee09 <jslee@ijeongsus-iMac.local>
2021-11-30 16:12:04 +0900

    Organize REPL Codes

--- test/InferenceEngineTests.ml
+++ test/InferenceEngineTests.ml
@@ -7,30 +7,29 @@ open ContextualFeatures
 open MakeGraph
 open SimilarityHandler
 open Loop
+open RulesOfInference
 
-module GraphTest = struct
-  let sample_graph =
-    G.empty
-    |> (fun graph -> G.add_edge graph ("nextLine", "26") ("create", "26"))
-    |> (fun graph -> G.add_edge graph ("create", "26") ("create", "31"))
-    |> (fun graph -> G.add_edge graph ("create", "31") ("run", "31"))
-    |> (fun graph -> G.add_edge graph ("run", "31") ("append", "32"))
-    |> (fun graph -> G.add_edge graph ("append", "32") ("run", "32"))
-    |> (fun graph -> G.add_edge graph ("run", "32") ("append", "32"))
-    |> (fun graph -> G.add_edge graph ("run", "32") ("toString", "32"))
-    |> (fun graph -> G.add_edge graph ("toString", "32") ("run", "32"))
-    |> (fun graph -> G.add_edge graph ("run", "32") ("batchUpdate", "34"))
-    |> fun graph -> G.add_edge graph ("run", "33") ("batchUpdate", "34")
+let json = Deserializer.deserialize_json ()
+
+let graph = GraphMaker.init_graph json
+
+let received_responses = []
+
+let nodewise_featuremap = NodeWiseFeatures.init_feature_map graph
+
+let test () = loop graph received_responses nodewise_featuremap 1
+
+let trunk_finder ~(start : G.LiteralVertex.t) ~(end_ : G.LiteralVertex.t) (graph : G.t) : trunk list
+    =
+  let all_trunks = identify_trunks graph in
+  List.filter
+    ~f:(fun trunk ->
+      Vertex.equal (G.LiteralVertex.to_vertex start graph) (List.hd_exn trunk)
+      && Vertex.equal (G.LiteralVertex.to_vertex end_ graph) (List.last_exn trunk) )
+    all_trunks
 
 
-  (** How will identify_trunk work on sample_graph? *)
-
-  let test () =
-    let trunks = identify_trunks sample_graph in
-    List.iter ~f:(fun trunk -> G.Trunk.pp trunk) trunks
-end
-
-module GraphMakerTest = struct
+module Notebook1 = struct
   open GraphRepr
 
   let json = Deserializer.deserialize_json ()
@@ -46,52 +45,7 @@ module GraphMakerTest = struct
   (* works well!!! *)
 end
 
-module DistMapTest = struct
-  let json = Deserializer.deserialize_json ()
-
-  let graph = GraphMaker.init_graph json
-
-  let distmap = make_map_for_graph graph
-
-  let vertices = G.all_vertices_of_graph graph
-
-  let distmap_keys = ProbMap.fold (fun k _ acc -> k :: acc) distmap []
-
-  let test () =
-    ProbMap.iter
-      (fun k v ->
-        Out_channel.output_string Out_channel.stdout @@ Vertex.to_string k ;
-        Out_channel.output_string Out_channel.stdout @@ ProbQuadruple.to_string v )
-      distmap
-
-
-  let test2 () =
-    List.for_all ~f:(fun elem -> List.mem ~equal:Vertex.equal vertices elem) distmap_keys
-
-
-  let test3 () =
-    List.for_all ~f:(fun elem -> List.mem ~equal:Vertex.equal distmap_keys elem) vertices
-
-  (* works well! *)
-end
-
-module TestLoop = struct
-  (* does it even run smoothly? *)
-
-  let json = Deserializer.deserialize_json ()
-
-  let graph = GraphMaker.init_graph json
-
-  let initial_distmap = make_map_for_graph graph
-
-  let received_responses = []
-
-  let nodewise_featuremap = NodeWiseFeatures.init_feature_map graph
-
-  let test () = loop initial_distmap received_responses graph nodewise_featuremap 1
-end
-
-module TestIO = struct
+module Notebook2 = struct
   let test () =
     Out_channel.output_string Out_channel.stdout "hihi how are you?: " ;
     Out_channel.flush Out_channel.stdout ;
@@ -100,7 +54,7 @@ module TestIO = struct
   (* oh we need to flush the out_channel first! *)
 end
 
-module TestSimilarityEdges = struct
+module Notebook3 = struct
   let json = Deserializer.deserialize_json ()
 
   let graph = GraphMaker.init_graph json
@@ -117,7 +71,7 @@ module TestSimilarityEdges = struct
   (* no... this is not working.... *)
 end
 
-module TestRedefineHandler = struct
+module Notebook4 = struct
   module ChainSliceSet = Set.Make (ChainSlice)
   module Util = Yojson.Basic.Util
 
@@ -167,7 +121,7 @@ end
 
 (* the repl makes tedious debugging very viable! *)
 
-module TrunkSimilarityTest = struct
+module Notebook5 = struct
   let json = Deserializer.deserialize_json ()
 
   let graph = GraphMaker.init_graph json
@@ -216,7 +170,7 @@ module TrunkSimilarityTest = struct
   (* --> all contextual extractors working. *)
 end
 
-module TestSimilarityEdges2 = struct
+module Notebook6 = struct
   (* 이제, sim edge가 왜 안 생기는지를 보자. *)
   let json = Deserializer.deserialize_json ()
 
@@ -256,99 +210,7 @@ module TestSimilarityEdges2 = struct
       nodewise_map
 end
 
-module TestNodewiseSim = struct
-  let json = Deserializer.deserialize_json ()
-
-  let graph = GraphMaker.init_graph json
-
-  let all_methods = G.all_methods_of_graph graph
-
-  open Out_channel
-
-  let _ = G.get_ns_edges graph
-
-  let make_nodewise_sim_edge (graph : G.t) : G.t =
-    let open SimilarVertexPairExtractor in
-    let all_vertices = G.all_vertices_of_graph graph in
-    let contextual_similarity_map =
-      NodewisePairExtractor.update_nodewise_similarity_map (G.all_methods_of_graph graph)
-    in
-    let above_threshold_entries =
-      NodeWiseSimilarityMap.filter
-        (fun _ similarity -> similarity >= NodewisePairExtractor.threshold)
-        contextual_similarity_map
-    in
-    NodeWiseSimilarityMap.fold
-      (fun (method1, method2) _ acc ->
-        let method1_vertices =
-          List.filter ~f:(fun (meth, _) -> String.equal meth method1) all_vertices
-        and method2_vertices =
-          List.filter ~f:(fun (meth, _) -> String.equal meth method2) all_vertices
-        in
-        (* we'll use smart_pairup_vertices to ensure we don't connect two distant vertices. *)
-        let smart_pairedup : (Vertex.t * Vertex.t) list =
-          ContextualPairExtractor.smart_pairup_vertices method1_vertices method2_vertices
-            (method1, method2)
-        in
-        List.fold
-          ~f:(fun smol_acc (v1, v2) ->
-            G.add_edge_e smol_acc (v1, EdgeLabel.NodeWiseSimilarity, v2)
-            |> fun graph -> G.add_edge_e graph (v2, EdgeLabel.NodeWiseSimilarity, v1) )
-          ~init:acc smart_pairedup )
-      above_threshold_entries graph
-
-
-  open SimilarVertexPairExtractor
-
-  let all_vertices = G.all_vertices_of_graph graph
-
-  let contextual_similarity_map =
-    NodewisePairExtractor.update_nodewise_similarity_map (G.all_methods_of_graph graph)
-
-
-  let above_threshold_entries =
-    NodeWiseSimilarityMap.filter
-      (fun _ similarity -> similarity >= NodewisePairExtractor.threshold)
-      contextual_similarity_map
-
-
-  (* let _ = *)
-  (*   NodeWiseSimilarityMap.iter *)
-  (*     (fun (k1, k2) v -> *)
-  (*       print_string k1 ; *)
-  (*       print_string k2 ; *)
-  (*       print_int v ; *)
-  (*       print_newline () ) *)
-  (*     above_threshold_entries *)
-
-  let testgraph = SimilarityHandler.EstablishSimEdges.make_nodewise_sim_edge graph
-
-  let _ = G.get_ns_edges testgraph
-
-  let method1 = "Map JdbcTemplate.queryForMap(String,Object[])"
-
-  let method2 = "int[] JdbcTemplate.batchUpdate(String,List)"
-
-  let method1_vertices = List.filter ~f:(fun (meth, _) -> String.equal meth method1) all_vertices
-
-  and method2_vertices = List.filter ~f:(fun (meth, _) -> String.equal meth method2) all_vertices
-
-  let smart_pairedup : (Vertex.t * Vertex.t) list =
-    ContextualPairExtractor.smart_pairup_vertices method1_vertices method2_vertices
-      (method1, method2)
-
-
-  let graph_added =
-    List.fold
-      ~f:(fun acc (v1, v2) ->
-        G.add_edge_e acc (v1, EdgeLabel.NodeWiseSimilarity, v2)
-        |> fun graph -> G.add_edge_e graph (v2, EdgeLabel.NodeWiseSimilarity, v1) )
-      ~init:graph smart_pairedup
-
-  (* pairwise similarity now working fine! *)
-end
-
-module TestContextualSimilarity = struct
+module Notebook7 = struct
   let json = Deserializer.deserialize_json ()
 
   let graph = GraphMaker.init_graph json
@@ -362,7 +224,7 @@ module TestContextualSimilarity = struct
   (* working fine *)
 end
 
-module TestAllSimilarities = struct
+module Notebook8 = struct
   let json = Deserializer.deserialize_json ()
 
   let graph = GraphMaker.init_graph json
@@ -383,129 +245,13 @@ module TestAllSimilarities = struct
   (* make_contextual_sim_edge is making a ("", "") vertex! *)
 end
 
-module TestContextualSimEdge = struct
-  let json = Deserializer.deserialize_json ()
-
-  let graph = GraphMaker.init_graph json
-
-  open SimilarVertexPairExtractor
-
-  let all_trunks = identify_trunks graph
-
-  let trunk_similarity_map = TrunkPairExtractor.update_trunk_similarity_map all_trunks
-
-  let _ = List.nth all_trunks 5
-
-  (* [("Map JdbcTemplate.queryForMap(String,Object[])", "{ line 37 }");
-     ("Map RelationalDataAccessApplication.query()", "{ line 37 }");
-     ("void RelationalDataAccessApplication.bridge()", "{ line 47 }");
-     ("void RelationalDataAccessApplication.printer(Map)", "{ line 41 }");
-     ("Collection Map.values()", "{ line 42 }");
-     ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }");
-     ("Iterator Collection.iterator()", "{ line 42 }");
-     ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }");
-     ("Object Iterator.next()", "{ line 42 }");
-     ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }");
-     ("StringBuilder StringBuilder.append(Object)", "{ line 43 }");
-     ("void RelationalDataAccessApplication.printer(Map)", "{ line 43 }")]
-
-      trunk list에 이게 왜 들어있는지 모르겠네... line43의 printer가 왜 리프? *)
-
-  (* 이제 왜 empty vertex가 생기는지를 봅시다. *)
-  (* 일단 similarity가 높은 trunk pair부터 찾아 봅시다. *)
-
-  let pair_with_max_value (map : TrunkSimilarityMap.t) =
-    fst
-    @@ TrunkSimilarityMap.fold
-         (fun key value (current_max_key, current_max_value) ->
-           if value > current_max_value then (key, value) else (current_max_key, current_max_value)
-           )
-         map
-         (([], []), Int.min_value)
-
-
-  let update_trunk_similarity_map (all_trunks : trunk list) : TrunkSimilarityMap.t =
-    let initial_map = TrunkSimilarityMap.init all_trunks in
-    TrunkSimilarityMap.fold
-      (fun ((t1, t2) as pair) _ acc ->
-        assert (not @@ G.Trunk.equal t1 t2) ;
-        let trunk_similarity = TrunkPairExtractor.get_trunk_similarity pair in
-        TrunkSimilarityMap.remove pair acc |> TrunkSimilarityMap.add pair trunk_similarity )
-      initial_map initial_map
-
-
-  let trunk_similarity_map = update_trunk_similarity_map all_trunks
-
-  let _ = pair_with_max_value trunk_similarity_map
-
-  let trunk1 =
-    [ ("Map JdbcTemplate.queryForMap(String,Object[])", "{ line 37 }")
-    ; ("Map RelationalDataAccessApplication.query()", "{ line 37 }")
-    ; ("void RelationalDataAccessApplication.bridge()", "{ line 47 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 41 }")
-    ; ("Collection Map.values()", "{ line 42 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }")
-    ; ("Iterator Collection.iterator()", "{ line 42 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }")
-    ; ("Object Iterator.next()", "{ line 42 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }")
-    ; ("boolean Iterator.hasNext()", "{ line 42 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }")
-    ; ("StringBuilder StringBuilder.append(Object)", "{ line 43 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 43 }")
-    ; ("String StringBuilder.toString()", "{ line 43 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 43 }") ]
-
-
-  let trunk2 =
-    [ ("Map JdbcTemplate.queryForMap(String,Object[])", "{ line 37 }")
-    ; ("Map RelationalDataAccessApplication.query()", "{ line 37 }")
-    ; ("void RelationalDataAccessApplication.bridge()", "{ line 47 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 41 }")
-    ; ("Collection Map.values()", "{ line 42 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }")
-    ; ("Iterator Collection.iterator()", "{ line 42 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }")
-    ; ("boolean Iterator.hasNext()", "{ line 42 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }")
-    ; ("Object Iterator.next()", "{ line 42 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 42 }")
-    ; ("StringBuilder StringBuilder.append(Object)", "{ line 43 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 43 }")
-    ; ("String StringBuilder.toString()", "{ line 43 }")
-    ; ("void RelationalDataAccessApplication.printer(Map)", "{ line 43 }") ]
-
-
-  (* ㅋㅋㅋㅋ 같은 줄 알았는데 아니었네. *)
-  (* 어쨌든 찾았다. *)
-
-  let _ = TrunkSimilarityMap.find (trunk1, trunk2) trunk_similarity_map
-
-  let trunk_pair = (trunk1, trunk2)
-
-  let contextually_similar_methods =
-    ContextualPairExtractor.identify_similar_method_from_similar_trunk trunk_pair graph
-
-
-  let smart_pairedup =
-    contextually_similar_methods
-    >>= fun (method1, method2) ->
-    ContextualPairExtractor.smart_pairup_vertices trunk1 trunk2 (method1, method2)
-
-
-  let testgraph =
-    List.fold
-      ~f:(fun smol_acc (v1, v2) -> G.add_edge_e smol_acc (v1, EdgeLabel.ContextualSimilarity, v2))
-      ~init:graph smart_pairedup
-end
-
-module TestInitGraph = struct
+module Notebook9 = struct
   let json = Deserializer.deserialize_json ()
 
   let graph = GraphMaker.init_graph json
 end
 
-module TestLoop2 = struct
+module Notebook10 = struct
   (* now it should run smoothly... *)
 
   let json = Deserializer.deserialize_json ()
@@ -519,7 +265,7 @@ module TestLoop2 = struct
   let test () = loop graph received_responses nodewise_featuremap 1
 end
 
-module InternalNodeNoneTest = struct
+module Notebook11 = struct
   let response1 = Response.ForLabel ("void PrintStream.println(String)", TaintLabel.Sink)
 
   let response2 =
@@ -557,32 +303,30 @@ module InternalNodeNoneTest = struct
   (* no... *)
 end
 
-module TestContextualBogus = struct
+module Notebook12 = struct
   (** given a trunk pair, get the pairs of methods that are contextually similar to each other. *)
-
-  let trunk_finder ~(start : G.LiteralVertex.t) ~(end_ : G.LiteralVertex.t) (graph : G.t) :
-      trunk list =
-    let all_trunks = identify_trunks graph in
-    List.filter
-      ~f:(fun trunk ->
-        Vertex.equal (G.LiteralVertex.to_vertex start graph) (List.hd_exn trunk)
-        && Vertex.equal (G.LiteralVertex.to_vertex end_ graph) (List.last_exn trunk) )
-      all_trunks
-
 
   let v1 = ("Map JdbcTemplate.queryForMap(String,Object[])", "{ line 37 }")
 
   let v2 = ("void PrintStream.println(String)", "{ line 43 }")
 
+  let v3 = ("Scanner.<init>(InputStream)", "{ line 24 }")
+
+  let v4 = ("int[] JdbcTemplate.batchUpdate(String,List)", "{ line 33 }")
+
   let _ = G.collect_df_roots graph
 
   let _ = G.collect_df_leaves graph
 
-  let _ = trunk_finder ~start:v1 ~end_:v2 graph
-
   let df_only_graph = G.leave_only_df_edges graph
 
+  let _ = trunk_finder ~start:v1 ~end_:v2 graph
+
+  let _ = trunk_finder ~start:v3 ~end_:v4 df_only_graph
+
   let _ = Visualizer.visualize_at_the_face df_only_graph
+
+  let _ = Visualizer.visualize_at_the_face graph
 
   let _ =
     PathUtils.is_reachable
@@ -619,3 +363,54 @@ module TestContextualBogus = struct
 
   (* find_path_from_source_to_dest is malfunctioning! *)
 end
+
+module Notebook13 = struct
+  let trunk1 =
+    [ ( "Map JdbcTemplate.queryForMap(String,Object[])"
+      , "{ line 37 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "Map RelationalDataAccessApplication.query()"
+      , "{ line 37 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "void RelationalDataAccessApplication.bridge()"
+      , "{ line 47 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "void RelationalDataAccessApplication.printer(Map)"
+      , "{ line 41 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "Collection Map.values()"
+      , "{ line 42 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "void RelationalDataAccessApplication.printer(Map)"
+      , "{ line 42 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "Iterator Collection.iterator()"
+      , "{ line 42 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "void RelationalDataAccessApplication.printer(Map)"
+      , "{ line 42 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "Object Iterator.next()"
+      , "{ line 42 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "void RelationalDataAccessApplication.printer(Map)"
+      , "{ line 42 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "StringBuilder StringBuilder.append(Object)"
+      , "{ line 43 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "void RelationalDataAccessApplication.printer(Map)"
+      , "{ line 43 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "String StringBuilder.toString()"
+      , "{ line 43 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "void RelationalDataAccessApplication.printer(Map)"
+      , "{ line 43 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} )
+    ; ( "void PrintStream.println(String)"
+      , "{ line 43 }"
+      , {InferenceEngineLib.GraphRepr.ProbQuadruple.src= 0.25; sin= 0.25; san= 0.25; non= 0.25} ) ]
+end
+
+module Notebook14 = struct end
