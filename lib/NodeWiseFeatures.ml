57c3067077a924f303f213a5bffdb8c7230263a3
jeongsoolee09 <volcano0909@gmail.com>
2021-12-02 18:05:04 +0900

    add some stuffs to NodeWiseFeatures.SingleFeature

--- lib/NodeWiseFeatures.ml
+++ lib/NodeWiseFeatures.ml
@@ -12,15 +12,29 @@ module SingleFeature = struct
   type t = string -> feature
 
   let int_of_feature feature =
-    match feature with FeatureMaps.NodeWiseFeatureMap.Int int -> int | _ -> failwith "not an int feature"
+    match feature with
+    | FeatureMaps.NodeWiseFeatureMap.Int int ->
+        int
+    | _ ->
+        failwith "not an int feature"
 
 
   let string_of_feature feature =
-    match feature with FeatureMaps.NodeWiseFeatureMap.String str -> str | _ -> failwith "not an string feature"
+    match feature with
+    | FeatureMaps.NodeWiseFeatureMap.String str ->
+        str
+    | _ ->
+        failwith "not an string feature"
 
 
   let bool_of_feature feature =
-    match feature with FeatureMaps.NodeWiseFeatureMap.Bool bool -> bool | _ -> failwith "not an bool feature"
+    match feature with
+    | FeatureMaps.NodeWiseFeatureMap.Bool bool ->
+        bool
+    | _ ->
+        failwith "not an bool feature"
+
+
   (** Pattern that captures (1) package name, (2) class name, and (3) method name from a unique
       identifier. *)
   let id_regex = Str.regexp "\\(.*\\)\\.?\\([A-Z][a-zA-Z$]+\\)\\.\\([a-zA-Z<>$]+\\)(.*)"
@@ -34,30 +48,29 @@ module SingleFeature = struct
     try
       assert (Str.string_match id_regex unique_identifier 0) ;
       String (Str.matched_group 1 unique_identifier)
-    with Assert_failure _ ->
-      failwith unique_identifier
+    with Assert_failure _ -> failwith unique_identifier
+
 
   let extract_class_name_from_id (unique_identifier : string) : feature =
     try
       assert (Str.string_match id_regex unique_identifier 0) ;
       String (Str.matched_group 2 unique_identifier)
-    with Assert_failure _ ->
-      failwith unique_identifier
+    with Assert_failure _ -> failwith unique_identifier
 
 
   let extract_method_name_from_id (unique_identifier : string) : feature =
     try
       assert (Str.string_match id_regex unique_identifier 0) ;
       String (Str.matched_group 3 unique_identifier)
-    with Assert_failure _ ->
-      failwith unique_identifier
+    with Assert_failure _ -> failwith unique_identifier
 
 
   let is_framework_code methname =
     let skip_methods = Deserializer.deserialize_skip_func ()
     and udf_methods = Deserializer.deserialize_method_txt () in
     let this_project_package_name =
-      string_of_feature @@ extract_package_name_from_id @@ List.hd_exn udf_methods in
+      string_of_feature @@ extract_package_name_from_id @@ List.hd_exn udf_methods
+    in
     let not_this_project_methods_and_java_methods =
       List.filter
         ~f:(fun str ->
@@ -67,15 +80,80 @@ module SingleFeature = struct
                (* should not belong to this package *) ) )
         skip_methods
     in
-    FeatureMaps.NodeWiseFeatureMap.Bool (List.mem ~equal:String.equal
-      (not_this_project_methods_and_java_methods >>| extract_method_name_from_id >>| string_of_feature)
-      (string_of_feature @@ extract_method_name_from_id methname))
+    FeatureMaps.NodeWiseFeatureMap.Bool
+      (List.mem ~equal:String.equal
+         ( not_this_project_methods_and_java_methods >>| extract_method_name_from_id
+         >>| string_of_feature )
+         (string_of_feature @@ extract_method_name_from_id methname) )
+
+
+  (** Pattern that captures (1) rtntype, (2) class name, and (3) method name from a unique
+      identifier. *)
+  let methstring_regex = Str.regexp "\\([a-zA-Z]*\\) ?\\([a-zA-Z$]+\\)\\.\\([a-zA-Z<>$]+\\)(.*)"
+
+  let extract_rtntype_from_methstring (methstring : string) : feature =
+    try
+      assert (Str.string_match methstring_regex methstring 0) ;
+      String (Str.matched_group 1 methstring)
+    with Assert_failure _ -> failwith methstring
+
+
+  let extract_class_name_from_methstring (methstring : string) : feature =
+    try
+      assert (Str.string_match methstring_regex methstring 0) ;
+      String (Str.matched_group 2 methstring)
+    with Assert_failure _ -> failwith methstring
+
+
+  let extract_method_name_from_methstring (methstring : string) : feature =
+    try
+      assert (Str.string_match methstring_regex methstring 0) ;
+      String (Str.matched_group 3 methstring)
+    with Assert_failure _ -> failwith methstring
+
+
+  let initstring_regex = Str.regexp "\\([a-zA-Z$]+\\)\\.\\([a-zA-Z<>$]+\\)(.*)"
+
+  let extract_rtntype_from_initstring (initstring : string) : feature =
+    try
+      assert (Str.string_match initstring_regex initstring 0) ;
+      String (Str.matched_group 1 initstring)
+    with Assert_failure _ -> failwith initstring
+
+
+  let extract_class_name_from_initstring (initstring : string) : feature =
+    try
+      assert (Str.string_match initstring_regex initstring 0) ;
+      String (Str.matched_group 2 initstring)
+    with Assert_failure _ -> failwith initstring
+
+
+  let extract_method_name_from_initstring (initstring : string) : feature =
+    try
+      assert (Str.string_match initstring_regex initstring 0) ;
+      String (Str.matched_group 3 initstring)
+    with Assert_failure _ -> failwith initstring
+
+
+  let find_unique_identifier_of_methstring (methstring : string) : string =
+    let methstring_classname = extract_class_name_from_methstring methstring
+    and methstring_method_name = extract_method_name_from_methstring methstring in
+    List.find_exn
+      ~f:(fun unique_id ->
+        String.is_substring
+          ~substring:
+            (F.asprintf "%s.%s"
+               (string_of_feature methstring_classname)
+               (string_of_feature methstring_method_name) )
+          unique_id )
+      (Deserializer.deserialize_method_txt () @ Deserializer.deserialize_skip_func ())
 end
 
 module PairwiseFeature = struct
   (* methname is the result of Procname.to_string *)
   let is_both_framework_code ((method1, method2) : string * string) : bool =
-    (SingleFeature.bool_of_feature @@ SingleFeature.is_framework_code method1) && (SingleFeature.bool_of_feature @@ SingleFeature.is_framework_code method2)
+    (SingleFeature.bool_of_feature @@ SingleFeature.is_framework_code method1)
+    && (SingleFeature.bool_of_feature @@ SingleFeature.is_framework_code method2)
 
 
   let belong_to_same_class ((method1, method2) : string * string) : bool =
@@ -90,15 +168,14 @@ module PairwiseFeature = struct
       (SingleFeature.string_of_feature (SingleFeature.extract_package_name_from_id method2))
 end
 
-let all_single_features = [
-  SingleFeature.extract_package_name_from_id
-; SingleFeature.extract_class_name_from_id
-; SingleFeature.extract_method_name_from_id
-; SingleFeature.is_framework_code
-]
+let all_single_features =
+  [ SingleFeature.extract_package_name_from_id
+  ; SingleFeature.extract_class_name_from_id
+  ; SingleFeature.extract_method_name_from_id
+  ; SingleFeature.is_framework_code ]
 
 
-let run_all_single_features (methname: string)  : SingleFeature.feature list =
+let run_all_single_features (methname : string) : SingleFeature.feature list =
   (* Possible TODO: change this to a dataframe *)
   List.rev
   @@ List.fold
@@ -112,5 +189,6 @@ let init_feature_map (graph : G.t) : FeatureMaps.NodeWiseFeatureMap.t =
   let all_methods = G.all_methods_of_graph graph in
   List.fold
     ~f:(fun acc meth ->
-        FeatureMaps.NodeWiseFeatureMap.strong_update acc meth (run_all_single_features meth) )
-    all_methods ~init:(FeatureMaps.NodeWiseFeatureMap.init graph)
+      FeatureMaps.NodeWiseFeatureMap.strong_update acc meth (run_all_single_features meth) )
+    all_methods
+    ~init:(FeatureMaps.NodeWiseFeatureMap.init graph)
