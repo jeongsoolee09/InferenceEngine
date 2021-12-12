8a918ba4c223988968a654e2cabae90fece91f01
jeongsoolee09 <jslee@ijeongsus-iMac.local>
2021-12-12 19:06:46 +0900

    run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+root_dir=/Users/jslee/InferenceEngine/
 bin_dir=/Users/jslee/InferenceEngine/_build/default/bin/
 
+cd $root_dir
+dune build
 cp *.txt $bin_dir
 cp *.json $bin_dir
 $bin_dir/InferenceEngineApplication.exe
