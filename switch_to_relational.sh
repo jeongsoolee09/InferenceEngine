4af1f3bea90b3d1202d1734a1d7c6fa7058cd250
jeongsoolee09 <jslee@ijeongsus-iMac.local>
2021-12-22 13:54:31 +0900

    scripts to switch btw relational, sagan

---
+++ switch_to_relational.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+lib="$(PWD)/lib"
+
+cp $PWD/resources/Annotations.json.relational $PWD/Annotations.json
+cp $PWD/resources/Annotations.json.relational $lib/Annotations.json
+cp $PWD/resources/Callgraph.txt.relational $PWD/Annotations.json
+cp $PWD/resources/Callgraph.txt.relational $lib/Annotations.json
+cp $PWD/resources/Chain.json.relational $PWD/Chain.json
+cp $PWD/resources/Chain.json.relational $lib/Chain.json
+cp $PWD/resources/GetterSetter.json.relational $PWD/GetterSetter.json
+cp $PWD/resources/GetterSetter.json.relational $lib/GetterSetter.json
+cp $PWD/resources/Methods.txt.relational $PWD/Methods.txt
+cp $PWD/resources/Methods.txt.relational $lib/Methods.txt
+cp $PWD/resources/skip_func.txt.relational $PWD/skip_func.txt
+cp $PWD/resources/skip_func.txt.relational $lib/skip_func.txt
