8a61cd99a9e4f944c00d55c3ac5cc919bf65707c
JeongSoo Lee <volcano0909@gmail.com>
2022-01-05 04:02:47 +0000

    debug run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 root_dir=$PWD
-bin_dir=$PWD/_build/default/bin/
+bin_dir="$PWD/_build/default/bin/"
 
 cd $root_dir
 dune build
-cp *.txt $bin_dir
+#cp *.txt $bin_dir
 cp *.json $bin_dir
 $bin_dir/InferenceEngineApplication.exe
