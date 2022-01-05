c27169dd17f49fdf9ecf89a91a1d92bcde317234
JeongSoo Lee <volcano0909@gmail.com>
2022-01-05 04:03:54 +0000

    simplify repr of Method

--- lib/Method.ml
+++ lib/Method.ml
@@ -2,28 +2,17 @@ open ListMonad
 open InfixOperators
 module F = Format
 
-type kind = UDF of {comp_unit: string; methname: string} | API of {methname: string}
-[@@deriving compare, equal, sexp]
+type t = String.t [@@deriving compare, equal, sexp]
 
-type t = NormalMethod of kind | Initializer of kind [@@deriving compare, equal, sexp]
+let dummy = "Dummy Dummy.dummy()"
 
-let dummy_kind : kind = UDF {comp_unit= ""; methname= "Dummy Dummy.dummy()"}
-
-let dummy = NormalMethod dummy_kind
-
-let get_kind (method_ : t) : kind =
-  match method_ with NormalMethod kind | Initializer kind -> kind
+let is_frontend (method_ : t) : bool =
+  String.is_substring ~substring:"$Lambda$" method_
+  || String.is_substring ~substring:"lambda$" method_
+  || String.is_prefix ~prefix:"__" method_
 
 
-let is_frontend (string : String.t) : bool =
-  String.is_substring ~substring:"$Lambda$" string
-  || String.is_substring ~substring:"lambda$" string
-  || String.is_prefix ~prefix:"__" string
-
-
-let is_initializer_methname (string : String.t) : bool =
-  String.is_substring ~substring:"<init>" string
-
+let is_initializer (method_ : t) : bool = String.is_substring ~substring:"<init>" method_
 
 module UniqueID = struct
   let id_regex = Str.regexp "\\(.*\\)\\.?\\([A-Z][a-zA-Z$0-9]+\\)\\.\\([a-zA-Z<>$0-9]+\\)(.*)"
@@ -41,6 +30,7 @@ module UniqueID = struct
       assert (Str.string_match id_regex unique_identifier 0) ;
       let match_ = Str.matched_group 2 unique_identifier in
       if String.is_substring ~substring:"$" match_ then
+        (* whoo it's a subclass *)
         String.take_while ~f:(fun char -> not @@ Char.equal '$' char) match_
       else match_
     with Assert_failure _ -> failwithf "extract_class_name_from_id: %s" unique_identifier ()
@@ -58,29 +48,29 @@ module NormalString = struct
       identifier. *)
   let normalstring_regex = Str.regexp "\\(.+\\) \\([a-zA-Z0-9$_]+\\)\\.\\([a-zA-Z<>0-9$_.]+\\)(.*)"
 
-  let get_return_type (methstring : String.t) : string =
+  let get_return_type (normalstring : String.t) : string =
     try
-      assert (Str.string_match normalstring_regex methstring 0) ;
-      Str.matched_group 1 methstring
-    with Assert_failure _ -> failwithf "extract_rtntype_from_normalstring: %s" methstring ()
+      assert (Str.string_match normalstring_regex normalstring 0) ;
+      Str.matched_group 1 normalstring
+    with Assert_failure _ -> failwithf "extract_rtntype_from_normalstring: %s" normalstring ()
 
 
-  let get_class_name (methstring : String.t) : string =
+  let get_class_name (normalstring : String.t) : string =
     try
-      assert (Str.string_match normalstring_regex methstring 0) ;
-      let match_ = Str.matched_group 2 methstring in
+      assert (Str.string_match normalstring_regex normalstring 0) ;
+      let match_ = Str.matched_group 2 normalstring in
       if String.is_substring ~substring:"$" match_ then
+        (* whoo it's a subclass *)
         String.take_while ~f:(fun char -> not @@ Char.equal '$' char) match_
       else match_
-    with Assert_failure _ -> failwithf "extract_class_name_from_normalstring: %s" methstring ()
+    with Assert_failure _ -> failwithf "extract_class_name_from_normalstring: %s" normalstring ()
 
 
-  let get_method_name (methstring : String.t) : string =
+  let get_method_name (normalstring : String.t) : string =
     try
-      assert (Str.string_match normalstring_regex methstring 0) ;
-      Str.matched_group 3 methstring
-    with Assert_failure _ ->
-      failwithf "extract_methstringname_from_normalstring: %s" methstring ()
+      assert (Str.string_match normalstring_regex normalstring 0) ;
+      Str.matched_group 3 normalstring
+    with Assert_failure _ -> failwithf "extract_method_name_from_normalstring: %s" normalstring ()
 end
 
 module InitString = struct
@@ -103,50 +93,30 @@ module InitString = struct
     with Assert_failure _ -> failwithf "extract_method_name_from_initstring: %s" initstring ()
 end
 
-let get_return_type_of_methname (methname : String.t) : string =
-  if is_initializer_methname methname then "" (* nothing, bro! *)
-  else NormalString.get_return_type methname
+let to_string (method_ : t) : string = method_
 
-
-let get_class_name_of_methname (methname : String.t) : string =
-  if is_initializer_methname methname then InitString.get_class_name methname
-  else NormalString.get_class_name methname
-
-
-let get_method_name_of_methname (methname : String.t) : string =
-  if is_initializer_methname methname then InitString.get_method_name methname
-  else NormalString.get_method_name methname
+let of_string (string : String.t) : t =
+  (* assert ( *)
+  (*   Str.string_match NormalString.normalstring_regex string 0 *)
+  (*   || Str.string_match InitString.initstring_regex string 0 ) ; *)
+  string
 
 
 let get_return_type (method_ : t) : string =
-  match method_ with
-  | NormalMethod (UDF {methname}) | NormalMethod (API {methname}) ->
-      NormalString.get_return_type methname
-  | Initializer (UDF {methname}) | Initializer (API {methname}) ->
-      ""
+  if is_initializer method_ then "" (* nothing, bro! *) else NormalString.get_return_type method_
 
 
 let get_class_name (method_ : t) : string =
-  match method_ with
-  | NormalMethod (UDF {methname}) | NormalMethod (API {methname}) ->
-      NormalString.get_class_name methname
-  | Initializer (UDF {methname}) | Initializer (API {methname}) ->
-      InitString.get_class_name methname
+  if is_initializer method_ then InitString.get_class_name method_
+  else NormalString.get_class_name method_
 
 
 let get_method_name (method_ : t) : string =
-  match method_ with
-  | NormalMethod (UDF {methname}) | NormalMethod (API {methname}) ->
-      NormalString.get_method_name methname
-  | Initializer (UDF {methname}) | Initializer (API {methname}) ->
-      InitString.get_method_name methname
+  if is_initializer method_ then InitString.get_method_name method_
+  else NormalString.get_method_name method_
 
 
-let string_list_to_string stringlist =
-  "[" ^ List.fold stringlist ~f:(fun acc string -> acc ^ string ^ ", ") ~init:"" ^ "]"
-
-
-let comp_unit_of_methname (methname : String.t) : String.t =
+let get_comp_unit (method_ : t) : String.t =
   let root_dir = Deserializer.deserialize_config () in
   let abs_dirs_and_classnames =
     DirectoryManager.Classnames.classnames_by_compilation_unit root_dir
@@ -154,89 +124,43 @@ let comp_unit_of_methname (methname : String.t) : String.t =
   let out =
     List.find
       ~f:(fun (abs_dir, classnames) ->
-        let class_name = get_class_name_of_methname methname in
-        List.mem classnames class_name ~equal:String.equal )
+        List.mem classnames (get_class_name method_) ~equal:String.equal )
       abs_dirs_and_classnames
   in
   match out with
   | None ->
-      failwithf "Could not find comp unit for %s" methname ()
+      failwithf "Could not find comp unit for %s" method_ ()
   | Some (abs_dir, _) ->
       List.last_exn @@ String.split ~on:'/' abs_dir
 
 
-let is_api_code_methname (methname : String.t) : bool =
+let is_api (method_ : t) : bool =
   try
-    let classname = get_class_name_of_methname methname
-    and methname = get_method_name_of_methname methname in
-    let classname_methname = F.asprintf "%s.%s" classname methname in
+    let classname = get_class_name method_ and methodname = get_method_name method_ in
+    let classname_methodname = F.asprintf "%s.%s" classname methodname in
     List.exists
-      ~f:(fun line -> String.is_substring ~substring:classname_methname line)
+      ~f:(fun line -> String.is_substring ~substring:classname_methodname line)
       (Deserializer.deserialize_skip_func ())
   with _ -> true
 
 
-let is_udf_code_methname (methname : String.t) : bool =
+let is_udf (method_ : t) : bool =
   try
-    let classname = get_class_name_of_methname methname
-    and methname = get_method_name_of_methname methname in
-    let classname_methname = F.asprintf "%s.%s" classname methname in
+    let classname = get_class_name method_ and methodname = get_method_name method_ in
+    let classname_methodname = F.asprintf "%s.%s" classname methodname in
     List.exists
-      ~f:(fun line -> String.is_substring ~substring:classname_methname line)
+      ~f:(fun line -> String.is_substring ~substring:classname_methodname line)
       (Deserializer.deserialize_method_txt ())
   with _ -> false
 
 
-let corner_case (methname : String.t) : bool =
-  (not @@ is_api_code_methname methname) && (not @@ is_udf_code_methname methname)
+let is_corner_case (method_ : t) : bool = (not @@ is_api method_) && (not @@ is_udf method_)
 
-
-let is_udf_code_of_this_comp_unit (method_ : t) (comp_unit : string) : bool =
-  match method_ with
-  | NormalMethod (UDF {comp_unit= this_comp_unit}) | Initializer (UDF {comp_unit= this_comp_unit})
-    ->
-      String.equal this_comp_unit comp_unit
-  | _ ->
-      false
-
-
-let to_string (method_ : t) : string =
-  match method_ with
-  | NormalMethod (UDF {methname})
-  | NormalMethod (API {methname})
-  | Initializer (UDF {methname})
-  | Initializer (API {methname}) ->
-      methname
-
-
-let is_api_method (method_ : t) : bool =
-  let methname = to_string method_ in
-  let classname = get_class_name_of_methname methname
-  and methname = get_method_name_of_methname methname in
-  let classname_methname = F.asprintf "%s.%s" classname methname in
-  List.exists
-    ~f:(fun line -> String.is_substring ~substring:classname_methname line)
-    (Deserializer.deserialize_skip_func ())
-
-
-let of_string (string : String.t) : t =
-  let kind : kind =
-    if is_udf_code_methname string then
-      UDF {comp_unit= comp_unit_of_methname string; methname= string}
-    else API {methname= string}
-  in
-  if is_initializer_methname string then Initializer kind else NormalMethod kind
-
-
-let find_unique_identifier_of_method (methname : String.t) : string =
-  let method_classname = get_class_name_of_methname methname
-  and method_method_name = get_method_name_of_methname methname in
+let find_unique_identifier (method_ : t) : string =
+  let method_classname = get_class_name method_ and method_name = get_method_name method_ in
   List.find_exn
     ~f:(fun unique_id ->
       String.is_substring
-        ~substring:(Format.asprintf "%s.%s" method_classname method_method_name)
+        ~substring:(Format.asprintf "%s.%s" method_classname method_name)
         unique_id )
     (Deserializer.deserialize_method_txt () @ Deserializer.deserialize_skip_func ())
-
-
-let is_initializer = function Initializer _ -> true | _ -> false
