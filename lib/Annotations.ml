9cbde6812467b3c3a3f3d615d57fc8e90ae1a210
jeongsoolee09 <jslee@ijeongsus-iMac.local>
2022-01-07 18:09:15 +0900

    remove REPL test codes

--- lib/Annotations.ml
+++ lib/Annotations.ml
@@ -28,21 +28,6 @@ let capture_angled_brackets (string : string) =
   |> trd3 >>| List.rev >>| String.of_char_list |> List.rev
 
 
-let big_sample =
-  "<_org.springframework.web.bind.annotation.RequestMapping(value=\"guides/{repositoryName}\", \
-   method=\".POST\", consumes=\"application/json\", produces=\"application/json\")> \
-   (<_org.springframework.web.bind.annotation.RequestBody> \
-   <_org.springframework.web.bind.annotation.RequestHeader(value=\"X-Hub-Signature\")> \
-   <_org.springframework.web.bind.annotation.RequestHeader(value=\"X-GitHub-Event\")> \
-   <_org.springframework.web.bind.annotation.PathVariable>)"
-
-
-let sample =
-  "<_org.springframework.web.bind.annotation.GetMapping(value=\"/{topical}/images/{image:[a-zA-Z0-9._-]+}\")>"
-
-
-let sample2 = "<_org.springframework.web.bind.annotation.PathVariable>"
-
 let split_single_annot_string (string : string) : string list =
   let regexp = Str.regexp "<\\([^<^(^)^>]+\\)\\(([^(^)]+)\\)*)?>" in
   assert (Str.string_match regexp string 0) ;
@@ -78,10 +63,4 @@ let single_annot_of_string : string -> single_annot =
   split_single_annot_string >> single_annot_of_splitted_string
 
 
-let x = split_single_annot_string sample
-
-let x = split_single_annot_string sample2
-
 let of_string (string : string) : t = string |> capture_angled_brackets >>| single_annot_of_string
-
-let x = of_string big_sample
