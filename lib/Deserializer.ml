915c84d8e77571ac5a56779445b59f16d3f51511
jeongsoolee09 <jslee@ijeongsus-iMac.local>
2022-01-07 20:41:06 +0900

    deserialize_json now caches on the disk, too

--- lib/Deserializer.ml
+++ lib/Deserializer.ml
@@ -5,9 +5,22 @@ open InfixOperators
 type json = Yojson.Basic.t
 
 module Json = Yojson.Basic
+module F = Format
 
 exception TODO
 (* NOTE: this module will become useless when integrated, so keep it simple! *)
+
+let make_now_string (gmt_diff : int) : string =
+  let open CalendarLib in
+  let now_raw = Calendar.now () in
+  let year = Calendar.year now_raw in
+  let month = Date.int_of_month @@ Calendar.month now_raw in
+  let day = Calendar.day_of_month now_raw in
+  let hour = Calendar.hour now_raw + gmt_diff in
+  let minute = Calendar.minute now_raw in
+  let second = Calendar.second now_raw in
+  F.asprintf "%d-%d-%d_%d:%d:%d" year month day hour minute second
+
 
 let deserialize_config () =
   let in_channel = In_channel.create "config.json" in
@@ -19,15 +32,36 @@ let deserialize_config () =
 
 let project_root = deserialize_config ()
 
+let yojson_already_serialized () =
+  Array.exists ~f:(fun dir -> String.is_substring ~substring:"yojson" dir) (Sys.readdir ".")
+
+
+let find_serialized_yojson () =
+  Array.find_exn ~f:(fun dir -> String.is_substring ~substring:"yojson" dir) (Sys.readdir ".")
+
+
 let deserialize_json =
   let cache : json ref = ref (`Assoc []) in
   fun () : json ->
     if Yojson.Basic.equal !cache (`Assoc []) then (
-      let in_channel = In_channel.create (project_root ^ "Chain.json") in
-      let out = Json.from_channel in_channel in
-      In_channel.close in_channel ;
-      cache := out ;
-      out )
+      if not @@ yojson_already_serialized () then (
+        let in_channel = In_channel.create (project_root ^ "Chain.json") in
+        let out = Json.from_channel in_channel in
+        In_channel.close in_channel ;
+        (* update the cache *)
+        cache := out ;
+        (* make a fresh marshal *)
+        let marshal_out_chan =
+          Out_channel.create (F.asprintf "%s_yojson.bin" (make_now_string 9))
+        in
+        Marshal.to_channel marshal_out_chan out [] ;
+        Out_channel.close marshal_out_chan ;
+        out )
+      else
+        let marshal_in_chan = In_channel.create (find_serialized_yojson ()) in
+        let from_marshal = Marshal.from_channel marshal_in_chan in
+        In_channel.close marshal_in_chan ;
+        from_marshal )
     else !cache
 
 
