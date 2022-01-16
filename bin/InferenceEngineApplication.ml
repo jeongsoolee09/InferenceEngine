aa22998d0e6671a1af69a3cb259b778da4c9a943
이정수 <jslee@ijeongsus-MacBook-Air.local>
2022-01-16 17:18:34 +0900

    Application itself is a dummy!!

--- bin/InferenceEngineApplication.ml
+++ bin/InferenceEngineApplication.ml
@@ -1,15 +1,4 @@
 open ListMonad
 open InfixOperators
-open GraphMaker
-open SimilarityHandler
 
-exception TODO
-
-let main () =
-  (* let graph = Deserializer.deserialize_json () |> GraphMaker.init_graph ~debug:true in *)
-  let json = Deserializer.deserialize_json () in
-  (* let interaction_completed = Loop.loop graph [] nodewise_featuremap 1 in *)
-  print_endline "done!"
-
-
-let _ = main ()
+let () = Main.main ()
