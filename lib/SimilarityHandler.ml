3bd33a15bad59a2857240891c6f45301e99a1f06
JeongSoo Lee <jeongsoolee@korea.ac.kr>
2022-01-30 14:01:58 +0900

    finish writing make_nodewise_sim_edge and make_contextual_sim_edge

--- lib/SimilarityHandler.ml
+++ lib/SimilarityHandler.ml
@@ -33,11 +33,39 @@ let make_nodewise_sim_edge (graph : G.t) : G.t =
   Out_channel.print_string "spawning python process compute_nodewise_similarity.py..." ;
   SpawnPython.spawn_python ~pyfile:"./lib/python/compute_nodewise_similarity.py" ~args:[] ;
   Out_channel.print_endline "done" ;
-  raise TODO
+  let ns_table =
+    ns_table_from_csv @@ F.asprintf "NodeWiseFeatures_%s_udfs.csv_filtered.csv" graph.comp_unit
+  in
+  Hashtbl.fold
+    (fun (m1, m2) _ current_graph ->
+      let m1_vertices = G.this_method_vertices current_graph m1
+      and m2_vertices = G.this_method_vertices current_graph m2 in
+      List.fold
+        ~f:(fun big_acc m1_vertex ->
+          List.fold
+            ~f:(fun smol_acc m2_vertex ->
+              G.add_edge_e smol_acc (m1_vertex, EdgeLabel.NodeWiseSimilarity, m2_vertex) )
+            ~init:big_acc m2_vertices )
+        ~init:graph m1_vertices )
+    ns_table graph
 
 
 let make_contextual_sim_edge (graph : G.t) : G.t =
   Out_channel.print_string "spawning python process compute_contextual_similarity.py..." ;
   SpawnPython.spawn_python ~pyfile:"./lib/python/compute_contextual_similarity.py" ~args:[] ;
   Out_channel.print_endline "done" ;
-  raise TODO
+  let cs_table =
+    cs_table_from_csv @@ F.asprintf "%s_all_longest_trunks.json_filtered.csv" graph.comp_unit
+  in
+  Hashtbl.fold
+    (fun (m1, m2) _ current_graph ->
+       let m1_vertices = G.this_method_vertices current_graph m1
+       and m2_vertices = G.this_method_vertices current_graph m2 in
+       List.fold
+         ~f:(fun big_acc m1_vertex ->
+             List.fold
+               ~f:(fun smol_acc m2_vertex ->
+                   G.add_edge_e smol_acc (m1_vertex, EdgeLabel.NodeWiseSimilarity, m2_vertex) )
+               ~init:big_acc m2_vertices )
+         ~init:graph m1_vertices )
+    cs_table graph
