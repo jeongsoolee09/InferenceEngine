c4183dabdc1c66ca723e166f98069d6075989bc7
이정수 <jeongsoolee@korea.ac.kr>
2022-03-02 17:03:07 +0900

    separate Question and Response to files

---
+++ lib/Question.ml
@@ -0,0 +1,26 @@
+open InfixOperators
+open ListMonad
+open GraphRepr
+
+type t = AskingForLabel of Method.t | AskingForConfirmation of (Method.t * TaintLabel.t)
+
+(** make a prompt message out of a question term. *)
+let make_prompt (question : t) : string =
+  match question with
+  | AskingForLabel meth ->
+      F.asprintf "What label does %s bear? [src|sin|san|non]: " (Method.to_string meth)
+  | AskingForConfirmation (meth, label) ->
+      F.asprintf "Method %s is a %s, right? [yes|no]: " (Method.to_string meth)
+        (TaintLabel.to_string label)
+
+
+let get_method (question : t) : Method.t =
+  match question with AskingForLabel meth -> meth | AskingForConfirmation (meth, _) -> meth
+
+
+let get_label (question : t) : TaintLabel.t =
+  match question with
+  | AskingForConfirmation (_, label) ->
+      label
+  | _ ->
+      raise @@ Invalid_argument "hahaha"
---
+++ lib/Response.ml
@@ -0,0 +1,71 @@
+open InfixOperators
+open ListMonad
+open GraphRepr
+open EdgeLabel
+
+type t = ForLabel of (Method.t * TaintLabel.t) | ForYesOrNo of (Method.t * TaintLabel.t * bool)
+
+let to_string (response : t) : string =
+  match response with
+  | ForLabel (meth_, label) ->
+      F.asprintf "ForLabel (%s, %s)" (Method.to_string meth_) (TaintLabel.to_string label)
+  | ForYesOrNo (meth_, label, bool) ->
+      F.asprintf "ForYesOrNo (%s, %s, %s)" (Method.to_string meth_) (TaintLabel.to_string label)
+        (Bool.to_string bool)
+
+
+let list_to_string (responses : t list) : string =
+  let contents =
+    List.fold responses ~f:(fun acc response -> acc ^ to_string response ^ ", ") ~init:""
+  in
+  F.asprintf "[%s]" contents
+
+
+let get_method (res : t) : Method.t =
+  match res with ForLabel (meth, _) -> meth | ForYesOrNo (meth, _, _) -> meth
+
+
+let get_label (res : t) : TaintLabel.t =
+  match res with
+  | ForLabel (_, label) ->
+      label
+  | _ ->
+      raise @@ Invalid_argument "this is not a response of a yes/no question"
+
+
+let get_yesorno (res : t) : bool =
+  match res with
+  | ForYesOrNo (_, _, bool) ->
+      bool
+  | _ ->
+      raise @@ Invalid_argument "this is not a response of a question asking for label"
+
+
+let response_of_dist (method_ : Method.t) (dist : ProbQuadruple.t) : t =
+  let label = ProbQuadruple.determine_label dist in
+  ForLabel (method_, label)
+
+
+let response_of_string_forlabel (method_ : Method.t) (response_str : string) : t =
+  match response_str with
+  | "src" | "source" ->
+      ForLabel (method_, TaintLabel.Source)
+  | "sin" | "sink" ->
+      ForLabel (method_, TaintLabel.Sink)
+  | "san" | "sanitizer" ->
+      ForLabel (method_, TaintLabel.Sanitizer)
+  | "non" | "none" ->
+      ForLabel (method_, TaintLabel.None)
+  | otherwise ->
+      raise @@ Invalid_argument otherwise
+
+
+let response_of_string_foryesorno (method_ : Method.t) (label : TaintLabel.t) (response_str : string)
+    : t =
+  match response_str with
+  | "yes" | "y" ->
+      ForYesOrNo (method_, label, true)
+  | "no" | "n" ->
+      ForYesOrNo (method_, label, false)
+  | otherwise ->
+      raise @@ Invalid_argument otherwise
