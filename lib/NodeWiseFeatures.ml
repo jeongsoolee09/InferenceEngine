7b863b656bd8be1ad904de0df04e5029026ae932
이정수 <jeongsoolee@korea.ac.kr>
2022-03-10 14:27:57 +0900

    wrap pairwisefeatures

--- lib/NodeWiseFeatures.ml
+++ lib/NodeWiseFeatures.ml
@@ -4,21 +4,19 @@ open GraphRepr
 
 (* open FeatureMaps *)
 open Method
-
 module F = Format
 module Hashtbl = Caml.Hashtbl
 
-type feature = Int of int | String of string | Bool of bool | Annotation of Annotations.t
+type feature_value = Int of int | String of string | Bool of bool | Annotation of Annotations.t
 [@@deriving equal]
 
 module SingleFeature = struct
-  type rule = Method.t -> feature
+  type feature = Method.t -> feature_value
 
-  type t = {label: string; rule: rule}
+  type t = {label: string; feature: feature}
 
   let this_project_package_name : string =
-    let skip_methods = Deserializer.deserialize_skip_func ()
-    and udf_methods = Deserializer.deserialize_method_txt () in
+    let udf_methods = Deserializer.deserialize_method_txt () in
     UniqueID.get_package_name @@ List.hd_exn udf_methods
 
 
@@ -107,7 +105,6 @@ module SingleFeature = struct
       match Hashtbl.find_opt cache method_ with
       | None ->
           let out =
-            let skip_func_lines = Deserializer.deserialize_skip_func () in
             let methstring = Method.to_string method_ in
             if String.is_substring ~substring:"<init>" methstring then
               is_java_builtin_class_initializer method_
@@ -186,42 +183,51 @@ module SingleFeature = struct
 
 
   let all_features : t array =
-    [| {label= "return_type"; rule= (fun m -> String (get_return_type m))}
-     ; {label= "class_name"; rule= (fun m -> String (get_class_name m))}
-     ; {label= "method_name"; rule= (fun m -> String (get_method_name m))}
-     ; {label= "package_name"; rule= (fun m -> String (get_package_name m))}
-     ; {label= "is_framework_method"; rule= (fun m -> Bool (is_framework_method m))}
-     ; {label= "is_java_builtin_method"; rule= (fun m -> Bool (is_java_builtin_method m))}
-     ; {label= "is_library_code"; rule= (fun m -> Bool (is_library_code m))}
+    [| {label= "return_type"; feature= (fun m -> String (get_return_type m))}
+     ; {label= "class_name"; feature= (fun m -> String (get_class_name m))}
+     ; {label= "method_name"; feature= (fun m -> String (get_method_name m))}
+     ; {label= "package_name"; feature= (fun m -> String (get_package_name m))}
+     ; {label= "is_framework_method"; feature= (fun m -> Bool (is_framework_method m))}
+     ; {label= "is_java_builtin_method"; feature= (fun m -> Bool (is_java_builtin_method m))}
+     ; {label= "is_library_code"; feature= (fun m -> Bool (is_library_code m))}
      ; { label= "returnval_not_used_in_caller"
-       ; rule= (fun m -> Bool (returnval_not_used_in_caller m)) }
-     ; {label= "is_initializer"; rule= (fun m -> Bool (is_initializer m))}
-     ; {label= "annots"; rule= (fun m -> Annotation (Annotations.get_annots m))} |]
+       ; feature= (fun m -> Bool (returnval_not_used_in_caller m)) }
+     ; {label= "is_initializer"; feature= (fun m -> Bool (is_initializer m))}
+     ; {label= "annots"; feature= (fun m -> Annotation (Annotations.get_annots m))} |]
 end
 
 module PairwiseFeature = struct
   (* methname is the result of Procname.to_string *)
   open SingleFeature
 
-  let is_both_framework_code ((method1, method2) : Method.t * Method.t) : bool =
+  type feature = Method.t * Method.t -> bool
+
+  type t = {label: string; feature: feature}
+
+  let is_both_framework_code : feature =
+   fun ((method1, method2) : Method.t * Method.t) : bool ->
     is_framework_method method1 && is_framework_method method2
 
 
-  let belong_to_same_class ((method1, method2) : Method.t * Method.t) : bool =
+  let belong_to_same_class : feature =
+   fun ((method1, method2) : Method.t * Method.t) : bool ->
     (not @@ (is_this_project_method method1 || is_this_project_method method2))
     && String.equal (get_class_name method1) (get_class_name method2)
 
 
-  let belong_to_same_package ((method1, method2) : Method.t * Method.t) : bool =
+  let belong_to_same_package : feature =
+   fun ((method1, method2) : Method.t * Method.t) : bool ->
     (not @@ (is_this_project_method method1 || is_this_project_method method2))
     && String.equal (Method.get_package_name method1) (Method.get_package_name method2)
 
 
-  let return_type_is_another's_class ((method1, method2) : Method.t * Method.t) : bool =
+  let return_type_is_another's_class : feature =
+   fun ((method1, method2) : Method.t * Method.t) : bool ->
     String.equal (get_return_type method1) (get_return_type method2)
 
 
-  let is_both_java_builtin ((method1, method2) : Method.t * Method.t) : bool =
+  let is_both_java_builtin : feature =
+   fun ((method1, method2) : Method.t * Method.t) : bool ->
     let method1_is_init = is_initializer method1 and method2_is_init = is_initializer method2 in
     match (method1_is_init, method2_is_init) with
     | true, true | false, false ->
@@ -230,12 +236,14 @@ module PairwiseFeature = struct
         false
 
 
-  let is_both_initializer ((method1, method2) : Method.t * Method.t) : bool =
+  let is_both_initializer : feature =
+   fun ((method1, method2) : Method.t * Method.t) : bool ->
     let method1_is_init = is_initializer method1 and method2_is_init = is_initializer method2 in
     method1_is_init && method2_is_init
 
 
-  let has_same_annots ((method1, method2) : Method.t * Method.t) : bool =
+  let has_same_annots : feature =
+   fun ((method1, method2) : Method.t * Method.t) : bool ->
     let method1_annots = Annotations.get_annots method1
     and method2_annots = Annotations.get_annots method2 in
     List.exists
@@ -245,20 +253,20 @@ module PairwiseFeature = struct
 
 
   let all_features =
-    [| is_both_framework_code
-     ; belong_to_same_class
-     ; belong_to_same_package
-     ; return_type_is_another's_class
-     ; is_both_java_builtin
-     ; is_both_initializer
-     ; has_same_annots |]
+    [| {label= "is_both_framework_code"; feature= is_both_framework_code}
+     ; {label= "belong_to_same_class"; feature= belong_to_same_class}
+     ; {label= "belong_to_same_package"; feature= belong_to_same_package}
+     ; {label= "return_type_is_another's_class"; feature= return_type_is_another's_class}
+     ; {label= "is_both_java_builtin"; feature= is_both_java_builtin}
+     ; {label= "is_both_initializer"; feature= is_both_initializer}
+     ; {label= "has_same_annots"; feature= has_same_annots} |]
 end
 
-let run_all_single_features (method_ : Method.t) : feature list =
+let run_all_single_features (method_ : Method.t) : feature_value list =
   List.rev
   @@ Array.fold
        ~f:(fun acc feature ->
-         let feature_value = feature.rule method_ in
+         let feature_value = feature.feature method_ in
          feature_value :: acc )
        SingleFeature.all_features ~init:[]
 
@@ -267,9 +275,9 @@ module NodeWiseFeatureMap = struct
   module WithMethodDomain = Caml.Map.Make (Method)
   include WithMethodDomain
 
-  type t = feature list WithMethodDomain.t
+  type t = feature_value list WithMethodDomain.t
 
-  let string_of_feature (feature : feature) : string =
+  let string_of_feature (feature : feature_value) : string =
     match feature with
     | Int int ->
         Int.to_string int
@@ -287,7 +295,9 @@ module NodeWiseFeatureMap = struct
 
   let init (methods : Method.t list) : t =
     List.fold
-      ~f:(fun current_map method_ -> add method_ (run_all_single_features method_) current_map)
+      ~f:(fun current_map method_ ->
+        print_endline method_ ;
+        add method_ (run_all_single_features method_) current_map )
       ~init:empty methods
 
 
@@ -302,17 +312,17 @@ module NodeWiseFeatureMap = struct
       if Sys.is_file_exn filename then
         print_endline @@ F.asprintf "File %s already exists. Skipping." filename
       else
-      let csv_repr =
-        headers
-        :: ( List.rev
-           @@ fold
-                (fun method_ features acc -> (method_ :: (features >>| string_of_feature)) :: acc)
-                featuremap [] )
-      in
-      let out_chan = Out_channel.create filename in
-      let csv_out_chan = Csv.to_channel ~quote_all:true out_chan in
-      Csv.output_all csv_out_chan csv_repr ;
-      Out_channel.close out_chan
+        let csv_repr =
+          headers
+          :: ( List.rev
+             @@ fold
+                  (fun method_ features acc -> (method_ :: (features >>| string_of_feature)) :: acc)
+                  featuremap [] )
+        in
+        let out_chan = Out_channel.create filename in
+        let csv_out_chan = Csv.to_channel ~quote_all:true out_chan in
+        Csv.output_all csv_out_chan csv_repr ;
+        Out_channel.close out_chan
   end
 
   let is_already_serialized filename =
